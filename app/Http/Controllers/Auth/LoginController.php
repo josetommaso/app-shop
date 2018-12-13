bfcc232cecb15aac8071aa36d74a0dc875c8df16
Jose Tommaso <tommaso_j@hotmail.com>
2018-12-13 20:33:36 +0000

    fixing bugs when adding products to cart

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request as IlluminateRequest;
 
 class LoginController extends Controller
 {
@@ -36,4 +37,18 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+   public function showLoginForm(IlluminateRequest $request) {
+      if ($request->has('redirect_to')) {
+         session()->put('redirect_to', $request->input('redirect_to'));
+      }
+
+      return view('auth.login');
+   }
+
+   public function redirectTo() {
+      if (session()->has('redirect_to'))
+         return session()->pull('redirect_to');
+      return $this->redirectTo;
+   }
 }
